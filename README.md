1c0f53127129eb327d7f51134a1356a340cc82f2
Rimian Perkins <hello@rimian.com.au>
2015-09-15 03:48:15 +1000

    requires

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Capybara Selectors for Angular Material
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+    gem 'capybara-angular-material'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require 'rack'
+require 'capybara'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
 
 RSpec.configure do |config|
   config.order = :random
