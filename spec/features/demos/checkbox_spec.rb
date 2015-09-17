177f2f6f083341fe176494df227483484ef37aed
Rimian Perkins <hello@rimian.com.au>
2015-09-17 17:05:53 +1000

    spec that it matches part of the text

--- spec/features/demos/checkbox_spec.rb
+++ spec/features/demos/checkbox_spec.rb
@@ -7,6 +7,10 @@ feature 'Angular Material Demos - Checkbox' do
   end
 
   scenario 'It has no Checkbox' do
-    expect(page).not_to have_md_checkbox 'A checkbox that does not exist'
+    expect(page).not_to have_md_checkbox 'checkbox that does not exist'
+  end
+
+  scenario 'It matches part of the text' do
+    expect('<md-checkbox>this is a checkbox with text in it</md-checkbox>').to have_md_checkbox('a checkbox')
   end
 end
