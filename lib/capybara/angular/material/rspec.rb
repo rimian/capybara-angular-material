971ad920aa86e73b33ca15b5b23a02ec9ba713fa
Rimian Perkins <hello@rimian.com.au>
2015-09-21 11:49:46 +1000

    initital radio button spec

--- lib/capybara/angular/material/rspec.rb
+++ lib/capybara/angular/material/rspec.rb
@@ -2,13 +2,17 @@ module Capybara
   module Angular
     module Material
       module RSpecMatchers
+        def have_md_button(locator)
+          HaveSelector.new(:xpath, "//button[contains(@class, 'md-button')]/span[normalize-space(text())='#{locator}']")
+        end
+
         def have_md_checkbox(locator, options={})
           checked = "[@aria-checked='#{options[:checked].to_s}']" if options.has_key?(:checked)
           HaveSelector.new(:xpath, "//md-checkbox#{checked}/*/span[normalize-space(text())='#{locator}']", options.reject {|k,v| k == :checked})
         end
 
-        def have_md_button(locator)
-          HaveSelector.new(:xpath, "//button[contains(@class, 'md-button')]/span[normalize-space(text())='#{locator}']")
+        def have_md_radio_button(locator)
+          HaveSelector.new(:xpath, "//md-radio-button/*/span[normalize-space(text())='#{locator}']")
         end
       end
     end
--- spec/features/demos/radio_spec.rb
+++ spec/features/demos/radio_spec.rb
@@ -4,6 +4,14 @@ feature 'Angular Material Demos - radio' do
     expect(page.find('h1')).to have_content 'Capybara Angular Material'
   end
 
-  it 'has a radio button'
-  it 'has no radio button'
+  it 'has no radio button' do
+    expect(page).not_to have_md_radio_button('This button does not exist')
+  end
+
+  it 'has a radio button with content' do
+    expect(page).to have_md_radio_button('Apple')
+  end
+
+  it 'has a radio button with value'
+  it 'has a selected radio button'
 end
