c68c6f35bb0ee10d2d8470995a80efee2e9167ab
Rimian Perkins <hello@rimian.com.au>
2015-09-21 11:13:15 +1000

    Add button controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,10 @@
 
 angular.module('app', ['ngMaterial', 'ngRoute']).config(function($routeProvider) {
-  $routeProvider.when('/button', {templateUrl: 'app/partials/button.html', controller: 'appController'});
+  $routeProvider.when('/button', {templateUrl: 'app/partials/button.html', controller: 'buttonCtrl'});
   $routeProvider.when('/checkbox', {templateUrl: 'app/partials/checkbox.html', controller: 'checkboxCtrl'});
   $routeProvider.when('/radio', {templateUrl: 'app/partials/radio.html', controller: 'appController'});
 }).controller('appController', function($scope) {
+}).controller('buttonCtrl', function($scope) {
   $scope.data = {
     group1 : 'Banana',
     group2 : '2',
