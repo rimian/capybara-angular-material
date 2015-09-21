9a9e46f802c8a3f289c4cf3cb3a1f7a21f50659b
Rimian Perkins <hello@rimian.com.au>
2015-09-21 11:27:17 +1000

    add radio button controller and fixture

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 angular.module('app', ['ngMaterial', 'ngRoute']).config(function($routeProvider) {
   $routeProvider.when('/button', {templateUrl: 'app/partials/button.html', controller: 'buttonCtrl'});
   $routeProvider.when('/checkbox', {templateUrl: 'app/partials/checkbox.html', controller: 'checkboxCtrl'});
-  $routeProvider.when('/radio', {templateUrl: 'app/partials/radio.html', controller: 'appController'});
+  $routeProvider.when('/radio', {templateUrl: 'app/partials/radio.html', controller: 'radioCtrl'});
 }).controller('appController', function($scope) {
 }).controller('buttonCtrl', function($scope) {
   $scope.title1 = 'Button';
@@ -16,4 +16,39 @@ angular.module('app', ['ngMaterial', 'ngRoute']).config(function($routeProvider)
   $scope.data.cb3 = false;
   $scope.data.cb4 = false;
   $scope.data.cb5 = false;
+}).controller('radioCtrl', function($scope) {
+  $scope.data = {
+    group1 : 'Banana',
+    group2 : '2',
+    group3 : 'avatar-1'
+  };
+  $scope.avatarData = [{
+      id: "avatars:svg-1",
+      title: 'avatar 1',
+      value: 'avatar-1'
+    },{
+      id: "avatars:svg-2",
+      title: 'avatar 2',
+      value: 'avatar-2'
+    },{
+      id: "avatars:svg-3",
+      title: 'avatar 3',
+      value: 'avatar-3'
+  }];
+  $scope.radioData = [
+    { label: '1', value: 1 },
+    { label: '2', value: 2 },
+    { label: '3', value: '3', isDisabled: true },
+    { label: '4', value: '4' }
+  ];
+  $scope.submit = function() {
+    alert('submit');
+  };
+  $scope.addItem = function() {
+    var r = Math.ceil(Math.random() * 1000);
+    $scope.radioData.push({ label: r, value: r });
+  };
+  $scope.removeItem = function() {
+    $scope.radioData.pop();
+  };
 });
