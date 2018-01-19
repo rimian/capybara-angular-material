ff405b667808ce8475079f9e28c64039a43ecdbf
Nguyễn Đức Long <duclong@ubc.vn>
2018-01-19 11:39:20 +0700

    Fix: XPath lookup

--- lib/capybara/angular/material/node/actions.rb
+++ lib/capybara/angular/material/node/actions.rb
@@ -4,16 +4,16 @@ module Capybara
       module Node
         module Actions
           def md_check(locator)
-            find(:xpath, "//md-checkbox/*/span[normalize-space(text())='#{locator}']").click
+            find(:xpath, "//md-checkbox/*/*[normalize-space(text())='#{locator}']").click
           end
 
           def md_uncheck(locator)
-            find(:xpath, "//md-checkbox/*/span[normalize-space(text())='#{locator}']").click
+            find(:xpath, "//md-checkbox/*/*[normalize-space(text())='#{locator}']").click
           end
 
           def md_select(value, options={})
             # trigger('click') prevents an overlapping element error.
-            find(:xpath, "//md-select/md-select-value/span[not(@class)][text()='#{options[:from]}']").trigger('click')
+            find(:xpath, "//md-select/md-select-value/*[not(@class)][text()='#{options[:from]}']").trigger('click')
             find(
               :xpath,
               %{
@@ -27,7 +27,7 @@ module Capybara
           private
 
           def select_md_text_xpath
-            "//md-select-value/span/*[contains(@class, 'md-text')]"
+            "//md-select-value/*/*[contains(@class, 'md-text')]"
           end
         end
       end
--- lib/capybara/angular/material/rspec.rb
+++ lib/capybara/angular/material/rspec.rb
@@ -5,11 +5,11 @@ module Capybara
         include ::Capybara::RSpecMatchers
 
         def have_md_button(locator)
-          HaveSelector.new(:xpath, "//button[contains(@class, 'md-button')]/span[normalize-space(text())='#{locator}']")
+          HaveSelector.new(:xpath, "//button[contains(@class, 'md-button')]/*[normalize-space(text())='#{locator}']")
         end
 
         def have_md_checkbox(locator, options={})
-          HaveSelector.new(:xpath, "//md-checkbox#{aria_checked(options)}#{ng_disabled(options)}/*/span[normalize-space(text())='#{locator}']")
+          HaveSelector.new(:xpath, "//md-checkbox#{aria_checked(options)}#{ng_disabled(options)}/*/*[normalize-space(text())='#{locator}']")
         end
 
         def have_md_list
@@ -21,11 +21,11 @@ module Capybara
         end
 
         def have_md_radio_button(locator, options={})
-          HaveSelector.new(:xpath, "//md-radio-button#{aria_checked(options)}/*/span[normalize-space(text())='#{locator}']")
+          HaveSelector.new(:xpath, "//md-radio-button#{aria_checked(options)}/*/*[normalize-space(text())='#{locator}']")
         end
 
         def have_md_select(locator, options={})
-          HaveSelector.new(:xpath, "//md-select/md-select-value/span[not(@class)][text()='#{locator}']")
+          HaveSelector.new(:xpath, "//md-select/md-select-value/*[not(@class)][text()='#{locator}']")
         end
 
         private
